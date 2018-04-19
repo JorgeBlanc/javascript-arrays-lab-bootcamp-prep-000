cee8d9ccb402d8d7eba6c6c3316c4e1d327b5dcc
George White <thegeorgewilliam@gmail.com>
2018-04-19 18:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield" ]
 
-function destructivelyAppendKitten(kittens) {
- var kittens = kittens.push()
+function destructivelyAppendKitten(kitten, name) {
+ kittens.push(name)
 }
